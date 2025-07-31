88c0cdda39865170bf08cdf0ab859635c1f347e8
Keesha Luthra <keeshaluthra2027@gmail.com>
2025-07-31 19:21:09 +0530

    Time: 69 ms (59.5%), Space: 14.3 MB (64.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -14,20 +14,30 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
         // return helper(0, -1, nums, dp);
 
-        for(int index = n - 1; index >= 0; index--){
-            for(int prev_index = index - 1; prev_index >= -1; prev_index--){
-                int notTake = 0;
-                notTake = dp[index + 1][prev_index + 1];
-                int take = 0;
-                if(prev_index == -1 || nums[index] > nums[prev_index]){
-                  take = 1 + dp[index + 1][index + 1];
+        // for(int index = n - 1; index >= 0; index--){
+        //     for(int prev_index = index - 1; prev_index >= -1; prev_index--){
+        //         int notTake = 0;
+        //         notTake = dp[index + 1][prev_index + 1];
+        //         int take = 0;
+        //         if(prev_index == -1 || nums[index] > nums[prev_index]){
+        //           take = 1 + dp[index + 1][index + 1];
+        //         }
+        //         dp[index][prev_index + 1] = max(notTake, take);
+        //     }
+        // }
+        // return dp[0][0];
+
+        vector<int>dp(n, 1);
+        for(int index = 0; index < n; index++){
+            for(int prev = 0; prev < index; prev++){
+                if(nums[prev] < nums[index]){
+                    dp[index] = max(dp[index], 1 + dp[prev]);
                 }
-                dp[index][prev_index + 1] = max(notTake, take);
             }
         }
-        return dp[0][0];
+        return *max_element(dp.begin(), dp.end());
     }
 };
