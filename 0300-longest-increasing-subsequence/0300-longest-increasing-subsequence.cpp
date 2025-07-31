4eab206d4382030031cf6223f64182ef2dfe4f63
Keesha Luthra <keeshaluthra2027@gmail.com>
2025-07-31 18:57:11 +0530

    Time: 274 ms (37.94%), Space: 296.4 MB (23.52%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,20 +1,33 @@
 class Solution {
-private:
-    int helper(int index, int prev_index, vector<int> &nums, vector<vector<int>> &dp){
-        if(index == nums.size()) return 0;
-        if(dp[index][prev_index + 1] != -1) return dp[index][prev_index + 1];
-        int notTake = 0 + helper(index + 1, prev_index, nums, dp);
-        int take = 0;
-        if(prev_index == -1 || nums[index] > nums[prev_index]){
-            take = 1 + helper(index + 1, index, nums, dp);
-        }
-        dp[index][prev_index + 1] = max(notTake, take);
-        return dp[index][prev_index + 1];
-    }
+// private:
+//     int helper(int index, int prev_index, vector<int> &nums, vector<vector<int>> &dp){
+//         if(index == nums.size()) return 0;
+//         if(dp[index][prev_index + 1] != -1) return dp[index][prev_index + 1];
+//         int notTake = 0 + helper(index + 1, prev_index, nums, dp);
+//         int take = 0;
+//         if(prev_index == -1 || nums[index] > nums[prev_index]){
+//             take = 1 + helper(index + 1, index, nums, dp);
+//         }
+//         dp[index][prev_index + 1] = max(notTake, take);
+//         return dp[index][prev_index + 1];
+//     }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
-        return helper(0, -1, nums, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        // return helper(0, -1, nums, dp);
+
+        for(int index = n - 1; index >= 0; index--){
+            for(int prev_index = index - 1; prev_index >= -1; prev_index--){
+                int notTake = 0;
+                notTake = dp[index + 1][prev_index + 1];
+                int take = 0;
+                if(prev_index == -1 || nums[index] > nums[prev_index]){
+                  take = 1 + dp[index + 1][index + 1];
+                }
+                dp[index][prev_index + 1] = max(notTake, take);
+            }
+        }
+        return dp[0][0];
     }
 };
