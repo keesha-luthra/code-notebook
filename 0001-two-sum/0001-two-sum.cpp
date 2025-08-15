9b4bb263dea1dd4f50e0ff4a01683cfcc2c7775f
Keesha Luthra <keeshaluthra2027@gmail.com>
2025-08-15 12:33:03 +0530

    Time: 147 ms (5.04%), Space: 14.1 MB (63.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,6 +4,7 @@ public:
         vector<int> ans;
         for(int i = 0; i < nums.size(); i++){
             for(int j = i + 1; j < nums.size(); j++){
+                if(i == j) continue;
                 if(nums[i] + nums[j] == target){
                     ans.push_back(i);
                     ans.push_back(j);
