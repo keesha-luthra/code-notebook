61156091dd8528e6b887869eeca82b94ac89b58a
Keesha Luthra <keeshaluthra2027@gmail.com>
2025-12-25 21:13:19 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.43%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,40 +1,17 @@
 class Solution {
-private:
-void swap(int &a, int &b){
-    int temp = a;
-    a = b;
-    b = temp;
-}
 public:
     void moveZeroes(vector<int>& nums) {
-        // vector<int> temp;
-        // for(int i = 0; i < nums.size(); i++){
-        //     if(nums[i] != 0) temp.push_back(nums[i]);
-        // }
-        // int s = temp.size();
-        // for(int i = 0; i < s; i++){
-        //     nums[i] = temp[i];
-        // }
-        // for(int i = s; i < nums.size(); i++){
-        //     nums[i] = 0;
-        // }
-        int i = 0;
-        int flag = 0;
-        for(int t = 0; t < nums.size(); t++){
-            if(nums[t] == 0) {
-                i = t;
-                flag = 1;
-                break;
+        int read = 0;
+        int write = -1;
+        while(read < nums.size()){
+            if(nums[read] != 0){
+                write++;
+                nums[write] = nums[read];
             }
+            read++;
         }
-        if(flag == 0) return;
-        int j = i + 1;
-        while(i < nums.size() && j < nums.size()){
-            if(nums[j] != 0){
-                swap(nums[i], nums[j]);
-                i++;
-            }
-            j++;
+        for(int i = write + 1; i < nums.size(); i++){
+            nums[i] = 0;
         }
     }
 };
