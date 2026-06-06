b62b15c97b1e97926d584f18b880d3ee69924dc4
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-06-06 20:16:00 +0530

    Time: 0 ms (100%), Space: 95.1 MB (79.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Ensure nums1 is the smaller array
-        if (nums1.size() > nums2.size())
+        int m = nums1.size(); 
+        int n = nums2.size();
+        if(m > n){
             return findMedianSortedArrays(nums2, nums1);
+        }
+        int low = 0;
+        int high = m;
+        int total_left = (m + n + 1) / 2;
+        double res = 0;
+        while(low <= high){
+            int i = low + (high - low) / 2;
+            int j = total_left - i;
+            double leftA, leftB, rightA, rightB = 0;
+            
+            if(i == 0) leftA = INT_MIN;
+            else leftA = nums1[i - 1];
+            
+            if(i == m) rightA = INT_MAX;
+            else rightA = nums1[i];
+            
+            if(j == 0) leftB = INT_MIN;
+            else leftB = nums2[j - 1];
+            
+            if(j == n) rightB = INT_MAX;
+            else rightB = nums2[j];
 
-        int x = nums1.size();
-        int y = nums2.size();
-        int low = 0, high = x;
+            if(leftA > rightB) high = i - 1;
 
-        while (low <= high) {
-            int partitionX = (low + high) / 2;
-            int partitionY = (x + y + 1) / 2 - partitionX;
+            else if(leftB > rightA) low = i + 1;
 
-            // If partition is 0 it means nothing is there on the left side. Use -INF
-            // If partition is length of array then there is nothing on the right side. Use +INF
-            int maxLeftX = (partitionX == 0) ? INT_MIN : nums1[partitionX - 1];
-            int minRightX = (partitionX == x) ? INT_MAX : nums1[partitionX];
-
-            int maxLeftY = (partitionY == 0) ? INT_MIN : nums2[partitionY - 1];
-            int minRightY = (partitionY == y) ? INT_MAX : nums2[partitionY];
-
-            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
-                // Correct partition found
-                if ((x + y) % 2 == 0) {
-                    return (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2.0;
-                } else {
-                    return max(maxLeftX, maxLeftY);
+            else{
+                if((m + n) % 2 != 0){
+                    res = max(leftA, leftB);
+                    return res;
                 }
-            } else if (maxLeftX > minRightY) {
-                // Move left in nums1
-                high = partitionX - 1;
-            } else {
-                // Move right in nums1
-                low = partitionX + 1;
+            
+                else{
+                    res = (max(leftA, leftB) + min(rightA, rightB)) / 2.0;
+                    return res;
+                }
             }
         }
-
-        // If we reach here, input arrays were not sorted
-        throw invalid_argument("Input arrays are not sorted");
+        return 0;
     }
 };
