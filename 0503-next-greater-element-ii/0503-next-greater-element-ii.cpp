b2836e4874b0d35fd62541325c73cb25f185cae8
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-06-09 10:05:43 +0530

    Time: 4 ms (58.05%), Space: 29.1 MB (14.1%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,43 +1,18 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        // int n = nums.size();
-        // vector<int> answer(n, -1);
-        // stack<int> st;
-
-        // for (int i = 2 * n - 1; i >= 0; i--) {
-        //     int current = nums[i % n];
-
-        //     while (!st.empty() && st.top() <= current) {
-        //         st.pop();
-        //     }
-
-        //     if (i < n) {
-        //         if (!st.empty()) {
-        //             answer[i] = st.top();
-        //         }
-        //     }
-
-        //     st.push(current);
-        // }
-
-        // return answer;
-        vector<int> answer;
-        int n = nums.size();
         stack<int> st;
-        for(int i = 2*n - 1; i >= 0; i--){
-            while(!st.empty() && st.top() <= nums[i % n]) {
-                st.pop();
-            }
+        vector<int> nge;
+        int n = nums.size();
+        for(int i = 2* n - 1; i >= 0; i--){
+            while(!st.empty() && st.top() <= nums[i % n]) st.pop();
             if(i < n){
-                if(st.empty()) answer.push_back(-1);
-                else {
-                    answer.push_back(st.top());
-                }
+                if(st.empty()) nge.push_back(-1);
+                else nge.push_back(st.top()); 
             }
             st.push(nums[i % n]);
         }
-        reverse(answer.begin(), answer.end());
-        return answer;
+        reverse(nge.begin(), nge.end());
+        return nge;
     }
 };
