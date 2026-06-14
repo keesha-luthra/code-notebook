26f87dc08c82607839a103b997bbc59d1d5f0b88
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-06-14 13:58:58 +0530

    Time: 58 ms (10.72%), Space: 75.6 MB (31.56%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummyNode = new ListNode(-1);
+        ListNode* temp = dummyNode;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        while(temp1 && temp2){
+            if(temp1->val <= temp2->val){
+                temp->next = temp1;
+                temp1 = temp1->next;
+            }
+            else{
+                temp->next = temp2;
+                temp2 = temp2->next;
+            }
+            temp = temp->next;
+        }
+        if(temp1){
+            temp->next = temp1;
+        }
+        else temp->next = temp2;
+        return dummyNode->next;
+    }
+    ListNode* findMiddle(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+    ListNode* sortList(ListNode* head) {
+        if(head == NULL || head->next == NULL) return head;
+
+        ListNode* mid = findMiddle(head);
+        ListNode* lefthead = head;
+        ListNode* righthead = mid->next;
+        mid->next = NULL;
+        
+        lefthead = sortList(lefthead);
+        righthead = sortList(righthead);
+        
+        return mergeTwoLists(lefthead, righthead);
+    }
+};
