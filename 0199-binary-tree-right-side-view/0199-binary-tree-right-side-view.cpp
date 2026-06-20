55fe2d002217fc3a5fd0dd52f389390a037c7942
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-06-21 01:04:47 +0530

    Time: 0 ms (100%), Space: 15 MB (64.16%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -13,21 +13,36 @@ class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
         // row->node;
-        map<int, int> mpp;
+        // map<int, int> mpp;
         // Node, row
-        queue<pair<TreeNode*, int>> q;
-        if(root) q.push({root, 0});
-        while(!q.empty()){
-            TreeNode* node = q.front().first;
-            int row = q.front().second;
-            q.pop();
-            mpp[row] = node->val;
-            if(node->left) q.push({node->left, row + 1});
-            if(node->right) q.push({node->right, row + 1});
-        }
+        // queue<pair<TreeNode*, int>> q;
+        // if(root) q.push({root, 0});
+        // while(!q.empty()){
+        //     TreeNode* node = q.front().first;
+        //     int row = q.front().second;
+        //     q.pop();
+        //     mpp[row] = node->val;
+        //     if(node->left) q.push({node->left, row + 1});
+        //     if(node->right) q.push({node->right, row + 1});
+        // }
+        // vector<int> ans;
+        // for(auto it : mpp){
+        //     ans.push_back(it.second);
+        // }
+        // return ans;
+
+        queue<TreeNode*> q;
+        if(root) q.push(root);
         vector<int> ans;
-        for(auto it : mpp){
-            ans.push_back(it.second);
+        while(!q.empty()){
+            int levelSize = q.size();
+            for(int i = 0; i < levelSize; i++){
+                TreeNode* node = q.front();
+                if(i == levelSize - 1) ans.push_back(node->val);
+                q.pop();
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
         }
         return ans;
     }
