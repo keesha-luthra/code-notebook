fef9ecf7bc8bd1855adbf35dff56e5951a1ed50c
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-06-22 13:09:35 +0530

    Time: 3 ms (82.05%), Space: 15.5 MB (49.65%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        vector<int> answer;
+        if(k == 0){
+            answer.push_back(target->val);
+            return answer;
+        }
+        unordered_map<TreeNode*, TreeNode*> mpp;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            q.pop();
+            if(temp->left){
+                mpp[temp->left] = temp;
+                q.push(temp->left);
+            }
+            if(temp->right){
+                mpp[temp->right] = temp;
+                q.push(temp->right);
+            }
+        }
+
+        set<TreeNode*> visited;
+        int currDistance = 0;
+        q.push(target);
+        visited.insert(target);
+        while(!q.empty()){
+            int levelSize = q.size();
+            for(int i = 0; i < levelSize; i++){
+                TreeNode* node = q.front();
+                q.pop();
+                if(node->left && visited.find(node->left) == visited.end()){
+                    q.push(node->left);
+                    visited.insert(node->left);
+                }
+                if(node->right && visited.find(node->right) == visited.end()){
+                    q.push(node->right);
+                    visited.insert(node->right);
+                }
+                if(mpp[node] && visited.find(mpp[node]) == visited.end()){
+                    q.push(mpp[node]);
+                    visited.insert(mpp[node]);
+                }
+            }
+            currDistance += 1;
+            if(currDistance == k) break;
+        }
+        while(!q.empty()){
+            answer.push_back(q.front()->val);
+            q.pop();
+        }
+        return answer;
+    }
+};
