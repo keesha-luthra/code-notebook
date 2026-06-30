1ce2fdff335739c43651bbb190096bd8f4329dda
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-06-30 20:09:14 +0530

    Time: 4 ms (55.01%), Space: 18.3 MB (70.64%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> findOrder(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(V);
+        for(int i = 0; i < edges.size(); i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+        }
+        vector<int> indegree(V, 0);
+        for(int i = 0; i < V; i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        queue<int> q;
+        for(int i = 0; i < V; i++){
+            if(indegree[i] == 0) q.push(i);
+        }
+        vector<int> ans;
+        int count = 0;
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            count++;
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        reverse(ans.begin(), ans.end());
+        if(count == V) return ans;
+        else return {};
+    }
+};
