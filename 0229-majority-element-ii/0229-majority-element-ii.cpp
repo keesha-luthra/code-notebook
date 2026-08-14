8763b1db9d7752fdf710c819f920192cc96bdc21
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-08-14 12:03:36 +0530

    Time: 0 ms (100%), Space: 23.7 MB (55.33%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -5,34 +5,31 @@ public:
         int candidate2 = 0;
         int count1 = 0;
         int count2 = 0;
-        vector<int> result;
         for(int i = 0; i < nums.size(); i++){
-            if(nums[i] == candidate1) count1++;
-            else if(nums[i] == candidate2) count2++;
-            else if(count1 == 0){
-                candidate1 = nums[i];
+            if(count1 == 0 && candidate2 != nums[i]){
                 count1 = 1;
+                candidate1 = nums[i];
             }
-            else if(count2 == 0){
-                candidate2 = nums[i];
+            else if(count2 == 0 && candidate1 != nums[i]){
                 count2 = 1;
+                candidate2 = nums[i];
             }
+            else if(nums[i] == candidate1) count1++;
+            else if(nums[i] == candidate2) count2++;
             else{
                 count1--;
                 count2--;
             }
         }
-
+        vector<int> ans;
         count1 = 0;
         count2 = 0;
         for(int i = 0; i < nums.size(); i++){
             if(nums[i] == candidate1) count1++;
             if(nums[i] == candidate2) count2++;
         }
-
-        if(count1 > nums.size()/3) result.push_back(candidate1);
-        if(candidate2 != candidate1 && count2 > nums.size()/3) result.push_back(candidate2);
-        
-        return result;
+        if(count1 > nums.size()/3) ans.push_back(candidate1);
+        if(candidate1 != candidate2 && count2 > nums.size()/3) ans.push_back(candidate2);
+        return ans;
     }
 };
