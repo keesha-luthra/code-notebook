a4fe6295d3482daaab6177e6c755367a15831f7a
Keesha Luthra <keeshaluthra2027@gmail.com>
2026-09-26 15:13:06 +0530

    Time: 0 ms (100%), Space: 15.1 MB (70.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,41 @@
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int start = 0;
-        int end = nums.size() - 1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
+        int left = 0;
+        int right = nums.size() - 1;
 
-            if(nums[mid] == target) return mid;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            // left half is sorted
-            if(nums[start] <= nums[mid]){
-                if(target >= nums[start] && target < nums[mid]){
-                    end = mid - 1;
-                }
-                else
-                    start = mid + 1;
+            if (nums[mid] == target) {
+                return mid;
             }
 
-            // right half is sorted 
-            else if(nums[mid] < nums[end]){
-                if(target > nums[mid] && target <= nums[end]){
-                    start = mid + 1;
+            // Check if the left half is sorted
+            if (nums[left] <= nums[mid]) {
+
+                // Is target inside the sorted left half?
+                if (nums[left] <= target &&
+                    target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
-                else
-                    end = mid - 1;
+
+            } else {
+                // The right half must be sorted
+
+                // Is target inside the sorted right half?
+                if (nums[mid] < target &&
+                    target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
+
         return -1;
     }
 };
